d1b5366f0c4563bc190aa41e792a598bb022092e
Kashif Ali <wgflyman90@gmail.com>
2026-05-06 10:01:54 +0500

    update pages

--- src/layouts/Pages.tsx
+++ src/layouts/Pages.tsx
@@ -2,8 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import Services from "../pages/Services/Service"
-// import Career from "../pages/Career/Career";
 import Contact from "../pages/Contact/Contact";
+import Career from "../pages/Career/Career";
 import NotFound from "./NotFound";
 
 export default function Pages() {
@@ -14,7 +14,7 @@ export default function Pages() {
       <Route path="/services" element={<Services />} />
       <Route path="/contact" element={<Contact />} />
 
-      {/* <Route path="/career" element={<Career />} /> */}
+      <Route path="/career" element={<Career />} />
       {/* 404 - Page Not Found */}
       <Route path="*" element={<NotFound />} />
     </Routes>
